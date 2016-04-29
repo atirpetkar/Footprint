e9d4ea0d20e7e1dfcc82f09878bb9004271497d7
Atir Petkar <atirpetkar@atirs-macbook-air.local>
2016-04-28 20:44:47 -0400

    Initial Commit

---
+++ Footprint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Footprint
+//
+//  Created by Atir Petkar on 4/28/16.
+//  Copyright © 2016 Atir Petkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
